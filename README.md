7deed868494375d5d68205fa5d965e393b6d5ec2
neander <b3xlon9@gmail.com>
2024-04-29 17:18:16 +0900

    auto_mouse_click_event - add keyboard event listener

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # rpa
 auto convert service from google colaboratory
 
+## auto
+
+#### auto mouse click event
+`auto_mouse/auto_click_event`
+
+<hr />
+
+## converter
+
 #### convert '.png' to '.jpg'
-`ImageConverter/convert_png_to_jpg`
+`image_converter/convert_png_to_jpg`
 
 #### convert '.etc' to '.png'
-`ImageConverter/convert_png`
+`image_converter/convert_png`
+
+#### convert image resize
+`image_converter/convert_image_resize`
--- auto_mouse/auto_click_event.py
+++ auto_mouse/auto_click_event.py
@@ -1,14 +1,27 @@
 import pyautogui
 import time
 from datetime import datetime, timedelta
+from pynput import keyboard
 import random
+import threading
 
 # I want to only click at last mouse position.
 # interval 30seconds
-time_interval = 30
+# time_interval = 30
+time_interval_input = input('input auto mouse click event interval time(min:10 seconds):').strip()
+time_interval = int(time_interval_input) if time_interval_input else 10
+
+while (True):
+    if (time_interval < 10):
+        time_interval = int(input('input auto mouse click event interval time(min:10 seconds):'))
+    else:
+        break
+
+pressed_keyboard_event = False
+
 format_time_reg = "%Y-%m-%d %H:%M:%S"
 
-
+# mouse click event
 def click_event():
     pyautogui.click(button="left")
     time.sleep(1)
@@ -16,6 +29,18 @@ def click_event():
 def double_click_event():
     pyautogui.doubleClick(button="left")
     time.sleep(1)
+
+# keyboard pressed event
+def pressed_event(key):
+    global pressed_keyboard_event
+
+    try:
+        if key == keyboard.Key.enter or key == keyboard.Key.space:
+            print()
+            print(f'{key.name} event. have good day!')
+            pressed_keyboard_event = True
+    except ArithmeticError:
+        pass
 
 
 if __name__ == "__main__":
@@ -27,8 +52,18 @@ if __name__ == "__main__":
     # print(f"click event time:${click_event_time_str}")
     print(f"start time:{start_time_str} >>> next click event time:{click_event_time_str}")
 
+    # observe keyboard event
+    keyboard_listener = keyboard.Listener(on_press=pressed_event)
+    keyboard_thread = threading.Thread(target=keyboard_listener.start)
+    keyboard_thread.start()
+
     # infinity click event
     while (True):
+        if pressed_keyboard_event == True:
+            keyboard_listener.stop()
+            keyboard_thread.join()
+            break
+
         current_time = datetime.now()
 
         if (current_time >= click_event_time):
@@ -43,4 +78,3 @@ if __name__ == "__main__":
             print(f'\r{current_time_str}', end='', flush=True)
 
         time.sleep(1)
-
