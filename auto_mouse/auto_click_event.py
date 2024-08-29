fbd6f5d6e2171c78c04a1ff7abe3335cbcedec53
Neander <b3xlon9@gmail.com>
2024-08-29 17:15:03 +0900

    Add annotation... I will add detect a mouse move motion

--- auto_mouse/auto_click_event.py
+++ auto_mouse/auto_click_event.py
@@ -7,7 +7,7 @@ import threading
 
 # I want to only click at last mouse position.
 # interval 30seconds
-# time_interval = 30
+# time_interval = 30 : 'event'가 발생하는 주기 설정
 time_interval_input = input('input auto mouse click event interval time(min:10 seconds):').strip()
 time_interval = int(time_interval_input) if time_interval_input else 10
 
@@ -64,11 +64,15 @@ if __name__ == "__main__":
             keyboard_thread.join()
             break
 
+        # current_time에 현재시간 기입
         current_time = datetime.now()
 
+        # 현재시간과 클릭한 시간 비교
         if (current_time >= click_event_time):
+            # 클릭한 시간이 현재 시간 이전이면 '마우스 좌클릭' 이벤트 발생
             click_event()
             
+            # 클릭한 이벤트 시간을 문자화 - 콘솔에 표시해주기 위함
             click_event_time = current_time + timedelta(seconds=time_interval)
             click_event_time_str = click_event_time.strftime(format_time_reg)
 
